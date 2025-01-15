7cc723e1674dab66bbc6f805850b780b7e82b8b4
frank <frank581.fgz@gmail.com>
2025-01-15 17:57:54 +0100

    Add useful files

--- Main.py
+++ Main.py
@@ -6,7 +6,7 @@ from tqdm import tqdm
 gym.register_envs(ale_py)
 # hyperparameters
 learning_rate = 0.01
-n_episodes = 100_000
+n_episodes = 100
 start_epsilon = 1.0
 epsilon_decay = start_epsilon / (n_episodes / 2)  # reduce the exploration over time
 final_epsilon = 0.1
@@ -22,15 +22,22 @@ agent = QBAgent(
     final_epsilon=final_epsilon,
 )
 
-for episode in tqdm(range(n_episodes)):
+history = []
+
+for episode in range(n_episodes):
     obs, info = env.reset()
     done = False
+    history.append(obs)
+    print(obs) 
 
     obs = tuple(obs)
     # play one episode
     while not done:
         action = agent.get_action(obs)
         next_obs, reward, terminated, truncated, info = env.step(action)
+        history.append(next_obs)
+
+        print(next_obs)
         # print(str(obs) + "- Lenght: " + str(len(obs)))
         # print(str(next_obs) +  "- Lenght: " + str(len(obs)))
 
@@ -44,3 +51,24 @@ for episode in tqdm(range(n_episodes)):
         obs = next_obs
 
     agent.decay_epsilon()
+
+"""
+history_sets = []
+history_count = []
+for i in range(128):
+    history_count.append(0)
+
+for i in range(128):
+    history_sets.append(set())
+
+for j in range(128):
+    for i in range(len(history)):
+        elem = history[i][j]
+        # print(j)
+        history_sets[j].add(elem)
+
+for i in range(128):
+    print("Position:" + str(i) + ", count:" + str(len(history_sets[i])))
+
+# print(history_sets)
+"""
