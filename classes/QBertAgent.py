b1f2e2d84fc17c1321c1f56a26447e8f9eccd52c
frank <frank581.fgz@gmail.com>
2025-01-15 20:48:47 +0100

    Add recording videos
    Co-authored-by: Annalaura Miglino <85029172+amigli@users.noreply.github.com>

--- classes/QBertAgent.py
+++ classes/QBertAgent.py
@@ -12,6 +12,7 @@ class QBAgent:
         epsilon_decay: float,
         final_epsilon: float,
         discount_factor: float = 0.95,
+        q_values = None
     ):
         """Initialize a Reinforcement Learning agent with an empty dictionary
         of state-action values (q_values), a learning rate and an epsilon.
@@ -25,7 +26,11 @@ class QBAgent:
             discount_factor: The discount factor for computing the Q-value
         """
         self.env = env
-        self.q_values = defaultdict(lambda: np.zeros(env.action_space.n))
+        
+        if q_values == None:
+            self.q_values = defaultdict(lambda: np.zeros(env.action_space.n))
+        else:
+            self.q_values = defaultdict(lambda: np.zeros(env.action_space.n), q_values)
 
         self.lr = learning_rate
         self.discount_factor = discount_factor
@@ -70,3 +75,7 @@ class QBAgent:
 
     def decay_epsilon(self):
         self.epsilon = max(self.final_epsilon, self.epsilon - self.epsilon_decay)
+
+    def get_Qvalues(self):
+        dictionary =  dict(self.q_values)
+        return dictionary
