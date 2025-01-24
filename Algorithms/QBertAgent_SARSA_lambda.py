62f15a9adb420a59507f6d3752f10463f59443a9
frank <frank581.fgz@gmail.com>
2025-01-24 23:20:33 +0100

    Add SARSA-Lambda

---
+++ Algorithms/QBertAgent_SARSA_lambda.py
@@ -0,0 +1,70 @@
+from collections import defaultdict
+import gymnasium as gym
+import numpy as np
+import pickle
+
+class SARSALambdaAgent:
+    def __init__(
+        self,
+        env: gym.Env,
+        learning_rate: float,
+        initial_epsilon: float,
+        epsilon_decay: float,
+        final_epsilon: float,
+        discount_factor: float = 0.95,
+        lambda_: float = 0.9,
+    ):
+        self.env = env
+        self.q_values = defaultdict(lambda: np.zeros(env.action_space.n))
+
+        self.lr = learning_rate
+        self.discount_factor = discount_factor
+        self.lambda_ = lambda_
+
+        self.epsilon = initial_epsilon
+        self.epsilon_decay = epsilon_decay
+        self.final_epsilon = final_epsilon
+
+        self.training_error = []
+        self.eligibility_traces = defaultdict(lambda: np.zeros(env.action_space.n))
+
+    def get_action(self, obs):
+        if np.random.random() < self.epsilon:
+            return self.env.action_space.sample()
+        else:
+            return int(np.argmax(self.q_values[obs]))
+
+    def update(
+        self,
+        obs,
+        action,
+        reward,
+        terminated,
+        next_obs,
+        next_action
+    ):
+
+        td_error = (
+            reward
+            + self.discount_factor * self.q_values[next_obs][next_action] * (1 - terminated)
+            - self.q_values[obs][action]
+        )
+
+        self.eligibility_traces[obs][action] += 1
+        # Backwardview = aggiorno gli stati passati in base all'egibility trace
+        for state_action, trace_value in self.eligibility_traces.items():
+            self.q_values[state_action] += self.lr * td_error * trace_value
+            self.eligibility_traces[state_action] *= self.discount_factor * self.lambda_
+
+        self.training_error.append(td_error)
+
+    def decay_epsilon(self):
+        self.epsilon = max(self.final_epsilon, self.epsilon - self.epsilon_decay)
+
+    def save_q_values(self, filename="q_values.pkl"):
+        with open(filename, "wb") as f:
+            pickle.dump(dict(self.q_values), f)
+
+    def load_q_values(self, filename="q_values.pkl"):
+        with open(filename, "rb") as f:
+            self.q_values = defaultdict(lambda: np.zeros(self.env.action_space.n), pickle.load(f))
---
+++ main_SARSA_lambda.py
@@ -0,0 +1,83 @@
+from Algorithms.QBertAgent_SARSA_lambda import SARSALambdaAgent
+import gymnasium as gym
+import ale_py
+from tqdm import tqdm
+from gymnasium.wrappers import RecordEpisodeStatistics, RecordVideo
+from EnvironmentWrappers.QbertObservationWrapper import QbertObservationWrapperTuple
+from EnvironmentWrappers.RewardFunction import RewardFunction
+gym.register_envs(ale_py)
+
+# hyperparameters
+learning_rate = 0.01
+n_episodes = 1200
+start_epsilon = 1.0
+epsilon_decay = start_epsilon / (n_episodes / 2)  # reduce the exploration over time
+final_epsilon = 0.1
+
+## TRAINING 
+env = gym.make("ALE/Qbert-ram-v5")  
+env =  QbertObservationWrapperTuple(env)
+agent = SARSALambdaAgent(
+    env=env,
+    learning_rate=learning_rate,
+    initial_epsilon=start_epsilon,
+    epsilon_decay=epsilon_decay,
+    final_epsilon=final_epsilon,
+)
+
+# history = []
+for episode in tqdm(range(n_episodes)):
+    obs, info = env.reset()
+    done = False
+    rewardFunction = RewardFunction(obs[0])
+    obs = obs[1]
+    action =  agent.get_action(obs)
+    while not done:
+        next_obs, reward, terminated, truncated, info = env.step(action)
+        reward = rewardFunction.calculate_reward(next_obs, reward)
+        next_obs = next_obs[1]
+        next_action = agent.get_action(next_obs)
+        # update the agent
+        agent.update(obs, action, reward, terminated, next_obs, next_action)
+
+        # update if the environment is done and the current obs
+        done = terminated or truncated
+        obs = next_obs
+        action =  next_action
+
+    agent.decay_epsilon()
+
+env.close()
+
+num_eval_episodes = 10
+
+env = gym.make("ALE/Qbert-ram-v5", render_mode="rgb_array")  
+env =  QbertObservationWrapperTuple(env)  
+env = RecordVideo(env, video_folder="videos_first", name_prefix="eval",
+                  episode_trigger=lambda x: True)
+env = RecordEpisodeStatistics(env, buffer_length=num_eval_episodes)
+
+
+for episode_num in range(num_eval_episodes):
+    obs, info = env.reset()
+    rewardFunction = RewardFunction(obs[0])
+    obs = obs[1]
+    done = False
+
+    while not done:
+        action = agent.get_action(obs)
+        next_obs, reward, terminated, truncated, info = env.step(action)
+        reward = rewardFunction.calculate_reward(next_obs, reward)
+        obs =  next_obs[1]
+
+        done = terminated or truncated
+env.close()
+
+print(f'Episode time taken: {env.time_queue}')
+print(f'Episode total rewards: {env.return_queue}')
+print(f'Episode lengths: {env.length_queue}')
+
+print("Training terminato")
+
+
+
