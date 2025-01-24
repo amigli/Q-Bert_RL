9e42cc3ad7c98da9a4413ba654840bffca08193c
frank <frank581.fgz@gmail.com>
2025-01-24 12:06:46 +0100

    Fix DQN

--- main_DQN.py
+++ main_DQN.py
@@ -2,7 +2,7 @@ import gymnasium as gym
 import ale_py
 from tqdm import tqdm
 from gymnasium.wrappers import RecordEpisodeStatistics, RecordVideo
-from classes.QbertObservationWrapper import QbertObservationWrapper
+from classes.QbertObservationWrapper import QbertObservationWrapperBox
 from classes.QBertAgent_DQN import DQNAgent
 from classes.RewardFunction import RewardFunction
 gym.register_envs(ale_py)
@@ -15,10 +15,10 @@ epsilon_decay = start_epsilon / (n_episodes / 2)
 final_epsilon = 0.1
 
 env = gym.make("ALE/Qbert-ram-v5")  
-env =  QbertObservationWrapper(env)
+env =  QbertObservationWrapperBox(env)
 obs, info = env.reset()
 
-state_dim = obs[1].shape[0]
+state_dim = obs[2].shape[0]
 action_dim = env.action_space.n
 agent = DQNAgent(state_dim, action_dim, lr=0.001, gamma=0.99, epsilon=1.0, epsilon_decay=0.995, buffer_size=10000)
 
@@ -29,16 +29,17 @@ for episode in tqdm(range(n_episodes)):
     obs, info = env.reset()
     done = False
     rewardFunction = RewardFunction(obs[0])
-    obs = obs[1]
+    obs = obs[2]
     total_reward = 0
     while not done:
         action = agent.act(obs)
         next_obs, reward, terminated, truncated, info = env.step(action)
     
         reward = rewardFunction.calculate_reward(next_obs, reward)
+        next_obs = next_obs[2]
 
         done =  terminated or truncated    
-
+        
         agent.remember(obs, action, reward, next_obs, done)
         obs = next_obs
         total_reward += reward
