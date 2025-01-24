f04cdfa72a9669afc64dd7827bde091175d50ca3
frank <frank581.fgz@gmail.com>
2025-01-24 21:46:42 +0100

    Fix A2C

--- main_A2C.py
+++ main_A2C.py
@@ -4,9 +4,7 @@ from stable_baselines3.common.evaluation import evaluate_policy
 import ale_py
 from tqdm import tqdm
 from gymnasium.wrappers import RecordEpisodeStatistics, RecordVideo
-from EnvironmentWrappers.QbertObservationWrapper import QbertObservationWrapperBox
-from EnvironmentWrappers.RewardFunction import RewardFunction
-from EnvironmentWrappers.RewardFunctionWrapper import RewardFunctionWrapper
+from EnvironmentWrappers.ObsRewardWrapper import ObsRewardWrapper
 from stable_baselines3.common.vec_env import SubprocVecEnv
 
 gym.register_envs(ale_py)
@@ -14,22 +12,20 @@ gym.register_envs(ale_py)
 def make_env(env_id):
     def _init():
         env = gym.make(env_id)  
-        env =  QbertObservationWrapperBox(env)
-
-        reward_function = RewardFunction(init_lives=3)
-
-        env = RewardFunctionWrapper(env, reward_function)
+        env = ObsRewardWrapper(env)
         return env
     return _init
 
-num_envs = 1
-envs = SubprocVecEnv([make_env("CartPole-v1") for _ in range(num_envs)])
 
-model = A2C("MlpPolicy", envs, verbose=1)
-model.learn(total_timesteps=100)
+if __name__ == '__main__':
+    num_envs = 1
+    envs = SubprocVecEnv([make_env("ALE/Qbert-ram-v5") for _ in range(num_envs)])
 
-print("Training terminato")
+    model = A2C("MlpPolicy", envs, verbose=1)
+    model.learn(total_timesteps=100)
 
-# Valutare il modello
-mean_reward, std_reward = evaluate_policy(model, env, n_eval_episodes=10)
-print(f"Ricompensa media: {mean_reward:.2f}, deviazione standard: {std_reward:.2f}")
+    print("Training terminato")
+
+    # Valutare il modello
+    mean_reward, std_reward = evaluate_policy(model, envs, n_eval_episodes=10)
+    print(f"Ricompensa media: {mean_reward:.2f}, deviazione standard: {std_reward:.2f}")
--- main_PPO.py
+++ main_PPO.py
@@ -10,7 +10,7 @@ gym.register_envs(ale_py)
 
 ## TRAINING 
 env = gym.make("ALE/Qbert-ram-v5")  
-env = ObsRewardWrapperc(env)
+env = ObsRewardWrapper(env)
 
 model = PPO(
     "MlpPolicy",      # Tipo di rete neurale (MLP per osservazioni vettoriali)
