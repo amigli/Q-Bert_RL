66e7f36eef0aea844149962cae8ff41b60a36143
frank <frank581.fgz@gmail.com>
2025-01-25 11:45:48 +0100

    Bug fix wrapper

--- EnvironmentWrappers/ObsRewardWrapper.py
+++ EnvironmentWrappers/ObsRewardWrapper.py
@@ -32,8 +32,13 @@ class ObsRewardWrapper(gym.Wrapper):
                 newobs.append(obs[j].rgb[2])
 
             # Inserimento delle posizioni dei dischi di salvataggio
-            newobs.append(obs[23].xy[0])
-            newobs.append(obs[23].xy[1])
+            if obs[23] != None :
+                # Inserimento delle posizioni dei dischi di salvataggio
+                newobs.append(obs[23].xy[0])
+                newobs.append(obs[23].xy[1])
+            else:
+                newobs.append(0)
+                newobs.append(0)
 
             if obs[24] != None :
                 # Inserimento delle coordinate di coily
