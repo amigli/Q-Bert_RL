0baab9e6199c6f7340174da1d5514936b5c7005c
frank <frank581.fgz@gmail.com>
2025-01-27 19:33:29 +0100

    Bug fix
    Co-authored-by: Annalaura Miglino <85029172+amigli@users.noreply.github.com>

--- Algorithms/QBertAgent_DQN.py
+++ Algorithms/QBertAgent_DQN.py
@@ -45,7 +45,7 @@ class DQNAgent:
     def act(self, state, train=True):
         state_tensor = torch.tensor(state, dtype=torch.float32).to(self.device).unsqueeze(0)
         if train and np.random.rand() <= self.epsilon:
-            if obs[0] == 74 and obs[1] == 17:
+            if state[0] == 74 and state[1] == 17:
                 sequence = [3,5]
                 return rand.choice(sequence)
             else:
